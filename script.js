76c6604c9b13f8e058efd2e81ff2d8c8f2d39e62
WilliamReisO <oliveira._r@hotmail.com>
2023-09-27 11:00:53 -0300

    mudança de script

--- script.js
+++ script.js
@@ -1,62 +1,41 @@
-const { eventNames } = require("process")
+let valorAtual = "";
+let operador = "";
+let resultado = 0;
 
-const calculadora = document.querySelector(' calculadora');
-const chaves = calculadora.querySelector('chaves');
+function adicionarNumero(numero) {
+    valorAtual += numero;
+    atualizarDisplay();
+}
 
-chaves.addEventListener('click' , event => {
-    if(event.target.matches('button')) {
-        const chave = event.target
-        const action = chave.dataset.action
-        const keyContent = chave.textContent
-        const displayedNum = display.textContent
+function operacao(op) {
+    operador = op;
+    resultado = parseFloat(valorAtual);
+    valorAtual = "" + operador;
+    atualizarDisplay();
+}
 
-        //remove a classe .is-depressed de todas as teclas
-        Array.from(chave.parentNode.children)
-             .forEach(chave.classList.remove('is-depressed'))
-    } 
-    
-   if(!action){
-    if(displayedNum === '0'){
-        display.textContent = chaveContent
-    }else {
-        display.textContent = displayedNum + chaveContent
+function calcular() {
+    if (operador === '+') {
+        resultado += parseFloat(valorAtual);
+    } else if (operador === '-') {
+        resultado -= parseFloat(valorAtual);
+    } else if (operador === '*') {
+        resultado *= parseFloat(valorAtual);
+    } else if (operador === '/') {
+        resultado /= parseFloat(valorAtual);
     }
-   }
+    valorAtual = resultado.toString();
+    operador = "";
+    atualizarDisplay();
+}
 
-    if(!action){
-        console.log('chave numerica !')
-    }
-    if(
-        action ==='add' ||
-        action ==='subtract' ||
-        action ==='multiply' ||
-        action ==='divide' 
-    ){
-        chave.classList.add('is-depressed')
-        //Adiciona um atributo personalizado 
-        calculadora.dataset.previousChaveType = 'operator'
-    }
-    if (action === 'decimal') {
-        display.textContent = displayedNum + '.'
-        console.log('chave decimal !')
-      }
-      
-      if (action === 'clear') {
-        console.log('chave clear !')
-      }
-      
-      if (action === 'calculate') {
-        console.log('chave equal !')
-      }
+function limpar() {
+    valorAtual = "";
+    operador = "";
+    resultado = 0;
+    atualizarDisplay();
+}
 
-      const previousChaveType = calculadora.dataset.previousChaveType
-
-      if(!action){
-        if(displayedNum === '0' || previousChaveType === 'operator'){
-            display.textContent = chaveContent
-        }else{
-            display.textContent = displayedNum + chaveContent
-        }
-      }
-})
-
+function atualizarDisplay() {
+    document.getElementById('display').value = valorAtual;
+}
